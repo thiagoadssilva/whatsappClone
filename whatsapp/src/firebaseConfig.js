7fdd8e26b8f3c5f68d666769f35d3c57935c032c
thiago jose da silva <thiago.ads.silva@gmail.com>
2020-11-12 22:01:55 -0300

    Criando os arquivos para fazer a conexao com firebase

---
+++ whatsapp/src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCpAdVHy0y5uTDoLgdVLlWK6B48gkN6G7M",
+    authDomain: "whatsappclone-9ec3b.firebaseapp.com",
+    databaseURL: "https://whatsappclone-9ec3b.firebaseio.com",
+    projectId: "whatsappclone-9ec3b",
+    storageBucket: "whatsappclone-9ec3b.appspot.com",
+    messagingSenderId: "959054986293",
+    appId: "1:959054986293:web:d1f3d5c30b81eb9e04c57c"
+  };
